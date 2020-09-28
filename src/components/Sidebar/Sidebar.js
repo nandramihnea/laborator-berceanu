a4480f6389baa7e249fc24a8054e3253024f1f75
Nandra Mihnea <nandra.mihnea@gmail.com>
2020-09-28 21:31:08 +0300

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,7 +5,7 @@ import classes from './Sidebar.module.css';
 const Sidebar = () => {
     return (
         <div className={classes.sidebar + " bg-yellow-200 p-6"}>
-            SIDEBARRRASD
+            Sidebar
         </div>
     )
 }
