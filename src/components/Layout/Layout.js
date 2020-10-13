d58ad3c092739d53a3f6f2d00debcf4f2a1a5a53
Nandra Mihnea <nandra.mihnea@gmail.com>
2020-10-13 20:00:57 +0300

    fix for build

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { useToggle } from '../../utils';
+// import { useToggle } from '../../utils';
 import SideDrawer from '../SideDrawer/SideDrawer';
 
 
 import classes from './Layout.module.css';
 
 const Layout = (props) => {
-    const [sideDrawerClosed, toggleSideDrawerClosed] = useToggle(false);
+    // const [sideDrawerClosed, toggleSideDrawerClosed] = useToggle(false);
 
     return (
         <div className={classes.layout}>
-            <SideDrawer closed={sideDrawerClosed}/>
+            <SideDrawer/>
             <main>
                 {props.children}
             </main>
