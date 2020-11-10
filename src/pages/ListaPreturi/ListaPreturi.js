a14a83a1ece67a31c8fa1d345616b8ead9a7aca7
Nandra Mihnea <nandra.mihnea@gmail.com>
2020-11-10 21:32:05 +0200

    lista preturi 2

--- src/pages/ListaPreturi/ListaPreturi.js
+++ src/pages/ListaPreturi/ListaPreturi.js
@@ -5,13 +5,30 @@ import {analyzes} from '../../assets/listaPreturi';
 import classes from './ListaPreturi.module.css';
 
 const ListaPreturi = () => {
+    const obj = {
+        1: 'Decontat cu trimitere de la Medicul de Familie',
+        2: 'Decontat cu trimitere de la Medicul Specialist',
+        3: 'Decontat cu trimitere de la Medicul de Familie sau Medicul Specialist',
+        4: 'Decontat cu trimitere de la Medicul de Familie (in cazul femeilor gravide) sau Medicul Specialist',
+    }
+
     const priceList = analyzes.map(type => {
         let content = (
             <div key={type.name} className={classes.list}>
                 <h3 className={classes.header + ' mr-5'}>{type.name}</h3>
                 <div>
                     {type.analyzes.map(analyze => {
-                        return <p key={analyze.id}>{analyze.name}</p>
+                        let row = (
+                            <div key={analyze.id} className={classes.row}>
+                                <p>{analyze.name}</p>
+                                <span>{obj[analyze.status]}</span>
+                                <div className="grid grid-cols-2 gap-x-1 text-right">
+                                    <span>{analyze.price}</span>
+                                    <span>Lei</span>
+                                </div>
+                            </div>
+                        )
+                        return row;
                     })}
                 </div>
             </div>
