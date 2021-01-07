1f8a320057ab31241669c9a9d25902b87e9ad138
Nandra Mihnea <nandra.mihnea@gmail.com>
2021-01-07 18:03:27 +0200

    contact page

---
+++ src/pages/Contact/Borzesti/Borzesti.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import ContactCard from '../ContactCard/ContactCard';
+
+const Borzesti = () => {
+    return (
+        <ContactCard
+            name='Laborator Clinic Dr. Berceanu SRL'
+            phone='0722 588 545'
+            email='rezultatelaboratorberceanu@gmail.com'
+            address='Sfinții Apostoli Petru și Pavel A84'
+            mapCode='https://goo.gl/maps/bDwpgqtAhRTDX9iy7'
+            position='justify-self-start' />
+    )
+};
+
+export default Borzesti;
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import classes from './Contact.module.css';
+
+import Borzesti from './Borzesti/Borzesti';
+import Teiului from './Teiului/Teiului';
+
 const Acreditari = () => {
     return (
         <div>
-            Contact
+            <div className="grid">
+                <Borzesti />
+                <Teiului />
+            </div>
         </div>
     )
 }
