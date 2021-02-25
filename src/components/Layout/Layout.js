4996dca504f63443fa82ab792341a695d34e83fa
Nandra Mihnea <nandra.mihnea@gmail.com>
2021-02-25 18:39:47 +0200

    background

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-
 import classes from './Layout.module.css';
+import {ReactComponent as Logo} from '../../assets/icons/logo.svg';
 
 const Layout = (props) => {
     return (
         <div className={classes.layout + ' relative text-neutral-9'}>
+            <Logo className={classes.logo} />
             {props.children}
         </div>
     )
