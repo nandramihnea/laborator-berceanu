396ff59957e274d494ae998e1da755407df9c0b5
Nandra Mihnea <nandra.mihnea@gmail.com>
2021-03-01 22:07:44 +0200

    context done

--- src/components/Navigation/NavItems/NavItems.js
+++ src/components/Navigation/NavItems/NavItems.js
@@ -3,8 +3,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from "framer-motion";
 
+import { useHomeContext, useHomeContextUpdate } from '../../../context/HomeContext';
 
 const NavItems = (props) => {
+    const isContactOpened = useHomeContext();
+    const toggleIsContactOpened = useHomeContextUpdate();
+
+    const handleContactClick = () => {
+        toggleIsContactOpened(isContactOpened);
+    }
+
     const variants = {
         hidden: {
             scale: 1,
@@ -30,7 +38,7 @@ const NavItems = (props) => {
                 initial='hidden'
                 whileHover='hover'
                 className={props.classes + ' p-4'} >
-                    <Link to='/' className="text-lg sm:text-base">Contact</Link>
+                    <div onClick={handleContactClick} className="text-lg sm:text-base cursor-pointer">Contact</div>
             </motion.li>
             <motion.li
                 variants={variants}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+
 import Footer from '../../components/Footer/Footer';
 import Hero from '../../components/Hero/Hero';
 import Navigation from '../../components/Navigation/Navigation';
+import { HomeProvider } from '../../context/HomeContext';
 import Acreditari from '../Acreditari/Acreditari';
 import ListaPreturi from '../ListaPreturi/ListaPreturi';
 
@@ -10,13 +12,15 @@ import classes from './Home.module.css';
 
 const Home = () => {
     return (
-        <div className={classes.home + ' relative'}>
-            <Navigation />
-            <Hero />
-            <Acreditari />
-            {/* <ListaPreturi /> */}
-            <Footer />
-        </div>
+        <HomeProvider>
+            <div className={classes.home + ' relative'}>
+                <Navigation />
+                <Hero />
+                <Acreditari />
+                {/* <ListaPreturi /> */}
+                <Footer />
+            </div>
+        </HomeProvider>
     )
 }
 
